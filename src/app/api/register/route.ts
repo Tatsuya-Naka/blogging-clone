c94f804bf8b816c705697ad77125d71ce7184145
Tatsu Nana <tatsunana@Tatsus-Air.lan>
2024-10-19 09:19:16 +1100

    Fixing bug

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { db } from "~/server/db";
 
 type reqDataType = {
