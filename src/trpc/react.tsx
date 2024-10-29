2be674906376c3de6d4d958663fd3bb2b7229365
Tatsu Nana <tatsunana@Tatsus-MacBook-Air.local>
2024-10-29 16:51:58 +1100

    Fix

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -73,5 +73,6 @@ function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
   // if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   if (process.env.RAILWAY_DEFAULT_URL) return `https://${process.env.RAILWAY_DEFAULT_URL}`;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  // return `http://localhost:${process.env.PORT ?? 3000}`;
+  return `http://localhost:${process.env.PORT ?? 8080}`;
 }
